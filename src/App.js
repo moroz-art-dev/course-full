9aad4ca182ffbd86c53f151ca0bcc6a2209c7481
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-18 00:01:49 +0300

    ClassComponent

--- src/App.js
+++ src/App.js
@@ -1,12 +1,12 @@
-import React, {useState} from "react";
-import Counter from "./components/Counter";
+import React from "react";
+import ClassCounter from "./components/ClassCounter";
 
 function App() {
     return (
         <div className="App">
-            <Counter/>
+            <ClassCounter/>
         </div>
-    );
+    )
 }
 
 export default App;
--- src/components/Counter.jsx
+++ src/components/Counter.jsx
@@ -10,11 +10,11 @@ const Counter = function () {
         setCount(count - 1)
     }
     return (
-        <>
-            <h1>{counter}</h1>
+        <div>
+            <h1>{count}</h1>
             <button onClick={increment}>Increment</button>
             <button onClick={decrement}>Decrement</button>
-        </>
+        </div>
     )
 }
 
