e96c163eec0bcd882c5990c1c38e1e698e7ed20c
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-19 02:23:53 +0300

    Uncontrolled components

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useRef, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import MyButton from "./components/UI/button/MyButton";
@@ -12,20 +12,29 @@ function App() {
         {id: 4, title: 'JavaScript', body: 'Description'}
     ])
     const [title, setTitle] = useState('')
+    const bodyInputRef = useRef()
     const addNewPost = (event) => {
         event.preventDefault()
+        console.log(title)
+        console.log(bodyInputRef.current.value)
     }
 
     return (
         <div className="App">
             <form>
+                {/*Controlled components*/}
                 <MyInput
                     value={title}
                     onChange={event => setTitle(event.target.value)}
                     type="text"
                     placeholder="Name"
                 />
-                <MyInput type="text" placeholder="Description"/>
+                {/*Uncontrolled components*/}
+                <MyInput
+                    ref={bodyInputRef}
+                    type="text"
+                    placeholder="Description"
+                />
                 <MyButton onClick={addNewPost}>Add post</MyButton>
             </form>
             <PostList post={post} title={'List'}/>
