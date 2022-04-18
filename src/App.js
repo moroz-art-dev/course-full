85939d345f659f9b32310543202b147ff91a6c09
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-19 01:44:39 +0300

    UI, Controlled components

--- src/App.js
+++ src/App.js
@@ -1,6 +1,8 @@
 import React, {useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
+import MyButton from "./components/UI/button/MyButton";
+import MyInput from "./components/UI/input/MyInput";
 
 function App() {
     const [post, setPost] = useState([
@@ -9,8 +11,23 @@ function App() {
         {id: 3, title: 'JavaScript', body: 'Description'},
         {id: 4, title: 'JavaScript', body: 'Description'}
     ])
+    const [title, setTitle] = useState('')
+    const addNewPost = (event) => {
+        event.preventDefault()
+    }
+
     return (
         <div className="App">
+            <form>
+                <MyInput
+                    value={title}
+                    onChange={event => setTitle(event.target.value)}
+                    type="text"
+                    placeholder="Name"
+                />
+                <MyInput type="text" placeholder="Description"/>
+                <MyButton onClick={addNewPost}>Add post</MyButton>
+            </form>
             <PostList post={post} title={'List'}/>
         </div>
     )
