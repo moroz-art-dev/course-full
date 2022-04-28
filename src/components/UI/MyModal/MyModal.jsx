2a3af6a1e295381d6c609c927db598229ed4ee87
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-28 18:03:31 +0300

    Modal window

---
+++ src/components/UI/MyModal/MyModal.jsx
@@ -0,0 +1,18 @@
+import React from 'react';
+import classes from './MyModal.module.css';
+
+const MyModal = ({children, visible, setVisible}) => {
+    const rootClasses = [classes.myModal]
+    if(visible){
+        rootClasses.push(classes.active)
+    }
+    return (
+        <div className={rootClasses.join(' ')} onClick={() => setVisible(false)}>
+            <div className={classes.myModalCenter} onClick={(event) => event.stopPropagation()}>
+                {children}
+            </div>
+        </div>
+    );
+};
+
+export default MyModal;
