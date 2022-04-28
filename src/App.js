a4bedd9c6ea66e730c88affde36fa4437cd9b1c8
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-28 18:43:21 +0300

    useEffect

--- src/App.js
+++ src/App.js
@@ -1,4 +1,4 @@
-import React, {useState} from "react";
+import React, {useEffect, useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
@@ -6,6 +6,7 @@ import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
 import {usePosts} from "./hooks/usePosts";
+import axios from "axios";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -18,11 +19,18 @@ function App() {
     const [modal, setModal] = useState(false)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-
+    useEffect(() => {
+        fetchPosts()
+    }, [])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
+    }
+
+    async function fetchPosts() {
+        const response = await axios.get('https://jsonplaceholder.typicode.com/posts')
+        setPosts(response.data)
     }
 
     const removePost = (post) => {
@@ -31,6 +39,7 @@ function App() {
 
     return (
         <div className="App">
+            <button onClick={fetchPosts}>GET</button>
             <MyButton style={{marginTop: '30px'}} onClick={() => setModal(true)}>
                 Create user
             </MyButton>
