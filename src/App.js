938e76e482530be6682389f3d61ed5f198c5e869
moroz-art-dev <moroz.art.dev@gmail.com>
2022-04-28 18:32:07 +0300

    decomposition, castom hooks usePosts

--- src/App.js
+++ src/App.js
@@ -1,10 +1,11 @@
-import React, {useMemo, useState} from "react";
+import React, {useState} from "react";
 import './styles/App.css'
 import PostList from "./components/PostList";
 import PostForm from "./components/PostForm";
 import PostFilter from "./components/PostFilter";
 import MyModal from "./components/UI/MyModal/MyModal";
 import MyButton from "./components/UI/button/MyButton";
+import {usePosts} from "./hooks/usePosts";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -15,17 +16,10 @@ function App() {
     ])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
 
-    const sortedPost = useMemo(() => {
-        if (filter.sort) {
-            return [...posts].sort((a, b) => a[filter.sort].localeCompare(b[filter.sort]))
-        }
-        return posts;
-    }, [filter.sort, posts])
 
-    const sortedAndSearchedPosts = useMemo(() => {
-        return sortedPost.filter(post => post.title.toLowerCase().includes(filter.query))
-    }, [filter.query, sortedPost])
+
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
         setModal(false)
--- src/components/PostList.jsx
+++ src/components/PostList.jsx
@@ -1,9 +1,10 @@
 import React from 'react';
 import PostItem from "./PostItem";
+import {TransitionGroup, CSSTransition} from "react-transition-group";
 
 const PostList = ({post, title, remove}) => {
-    if(!post.length){
-        return  (
+    if (!post.length) {
+        return (
             <h1 style={{textAlign: "center"}}>Posts not found!</h1>
         )
     }
@@ -11,9 +12,17 @@ const PostList = ({post, title, remove}) => {
     return (
         <div>
             <h1 style={{textAlign: 'center'}}>{title}</h1>
-            {post.map((post, index) =>
-                <PostItem remove={remove} number={index + 1} key={post.id} post={post}/>
-            )}
+            <TransitionGroup>
+                {post.map((post, index) =>
+                    <CSSTransition
+                        key={post.id}
+                        timeout={500}
+                        classNames="post"
+                    >
+                    <PostItem remove={remove} number={index + 1} post={post}/>
+                    </CSSTransition>
+                )}
+            </TransitionGroup>
         </div>
     );
 };
