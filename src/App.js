9f8c0cabb05a2f0f8a96d0b78f02c2bc3d3a4942
moroz-art-dev <moroz.art.dev@gmail.com>
2022-05-02 01:35:57 +0300

    pagination

--- src/App.js
+++ src/App.js
@@ -9,6 +9,7 @@ import {usePosts} from "./hooks/usePosts";
 import PostService from "./API/PostService";
 import Loader from "./components/UI/Loader/Loader";
 import {useFetching} from "./hooks/useFetching";
+import {getPagesArray, getPagesCount} from "./utils/pages";
 
 function App() {
     const [posts, setPosts] = useState([
@@ -19,15 +20,22 @@ function App() {
     ])
     const [filter, setFilter] = useState({sort: '', query: ''})
     const [modal, setModal] = useState(false)
+    const [totalPages, setTotalPages] = useState(0)
+    const [limit, setLimit] = useState(10)
+    const [page, setPage] = useState(1)
     const sortedAndSearchedPosts = usePosts(posts, filter.sort, filter.query)
-    const [fetchPosts, isPostLoading, postError ] = useFetching(async () => {
-        const posts = await PostService.getAll()
-        setPosts(posts)
+    let pagesArray = getPagesArray(totalPages)
+
+    const [fetchPosts, isPostLoading, postError] = useFetching(async () => {
+        const response = await PostService.getAll(limit, page)
+        setPosts(response.data)
+        const totalCount = response.headers['x-total-count']
+        setTotalPages(getPagesCount(totalCount, limit))
     })
 
     useEffect(() => {
         fetchPosts()
-    }, [])
+    }, [page])
 
     const createPost = (newPost) => {
         setPosts([...posts, newPost])
@@ -36,6 +44,10 @@ function App() {
 
     const removePost = (post) => {
         setPosts(posts.filter(p => p.id !== post.id))
+    }
+
+    const changePage = (page) => {
+        setPage(page)
     }
 
     return (
@@ -59,7 +71,17 @@ function App() {
                 ? <div style={{display: 'flex', justifyContent: 'center', marginTop: 50}}><Loader/></div>
                 : <PostList remove={removePost} post={sortedAndSearchedPosts} title={'List'}/>
             }
-
+            <div className="page__wrapper">
+                {pagesArray.map(p =>
+                    <span
+                        onClick={() => changePage(p)}
+                        className={page === p ? 'page__current page' : 'page'}
+                        key={p}
+                    >
+                        {p}
+                    </span>
+                )}
+            </div>
         </div>
     )
 }
--- src/components/PostItem.jsx
+++ src/components/PostItem.jsx
@@ -5,7 +5,7 @@ const PostItem = (props) => {
     return (
         <div className="post">
             <div className="post__content">
-                <strong>{props.number}. {props.post.title}</strong>
+                <strong>{props.post.id}. {props.post.title}</strong>
                 <p>{props.post.body}</p>
             </div>
             <div className="post__btn">
